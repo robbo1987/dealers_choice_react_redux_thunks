0f73084bac1dab19c50d721564f27537c1a676c0
Robert Albenda <ralbenda@gmail.com>
2022-03-13 17:15:31 -0400

    got some front end working

--- server.js
+++ server.js
@@ -26,7 +26,8 @@ const Guitar = sequelize.define("guitar", {
   },
 });
 
-Guitarist.belongsTo(Guitar)
+Guitarist.belongsTo(Guitar);
+Guitar.hasMany(Guitarist);
 //express
 const express = require("express");
 const app = express();
@@ -36,26 +37,36 @@ const port = process.env.PORT || 3000;
 app.use("/dist", express.static(path.join(__dirname, "dist")));
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
-app.get('/api/guitarists', async (req,res,next) => {
-    try { const guitarists = await Guitarist.findAll()
-           res.send(guitarists) 
+app.get("/api/guitarists", async (req, res, next) => {
+  try {
+    const guitarists = await Guitarist.findAll({
+      include: [Guitar],
+    });
+    res.send(guitarists);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.get("/api/guitars", async (req, res, next) => {
+  try {
+    const guitars = await Guitar.findAll({
+      include: [Guitarist],
+    });
+    res.send(guitars);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.get('/', async (req,res,next) => {
+    try{
+
     }
-    catch(ex){
-            next(ex)
+    catch(ex) {
+        next(ex)
     }
 })
-
-app.get('/api/guitars', async (req,res,next) => {
-    try { const guitars = await Guitar.findAll()
-           res.send(guitars) 
-    }
-    catch(ex){
-            next(ex)
-    }
-})
-
-
-
 //start function
 
 const init = async () => {
@@ -67,10 +78,13 @@ const init = async () => {
       tedN,
       joeW,
       riversC,
+      tomMor,
+      keithR,
+      ericC,
       fenderStrat,
       fenderTele,
       gibsonLesPaul,
-      gibsonFlyingV,
+      gibsonByrdland,
       espEclipse,
       espHorizon,
     ] = await Promise.all([
@@ -79,17 +93,35 @@ const init = async () => {
       Guitarist.create({ name: "Ted Nugent" }),
       Guitarist.create({ name: "Joe Walsh" }),
       Guitarist.create({ name: "Rivers Cuomo" }),
+      Guitarist.create({ name: "Tom Morello" }),
+      Guitarist.create({ name: "Keith Richards" }),
+      Guitarist.create({ name: "Eric Clapton" }),
       Guitar.create({ name: "Fender Stratocaster" }),
       Guitar.create({ name: "Fender Telecaster" }),
       Guitar.create({ name: "Gibson Les Paul" }),
-      Guitar.create({ name: "Gibson Flying v" }),
+      Guitar.create({ name: "Gibson Byrdland" }),
       Guitar.create({ name: "ESP Eclipse" }),
       Guitar.create({ name: "ESP Horizon" }),
     ]);
     jamesH.guitarId = espEclipse.id;
+    kirkH.guitarId = espHorizon.id;
+    tedN.guitarId = gibsonByrdland.id;
+    joeW.guitarId = gibsonLesPaul.id;
+    riversC.guitarId = fenderStrat.id;
+    tomMor.guitarId = fenderTele.id;
+    keithR.guitarId = fenderTele.id;
+    ericC.guitarId = fenderStrat.id;
+
     await Promise.all([
-        jamesH.save()
-    ]) 
+      jamesH.save(),
+      kirkH.save(),
+      tedN.save(),
+      joeW.save(),
+      riversC.save(),
+      tomMor.save(),
+      ericC.save(),
+      keithR.save(),
+    ]);
     app.listen(port, () => console.log(`listening on port ${port}`));
   } catch (ex) {
     console.log(ex);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,36 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
+import axios from "axios";
 
-render(<hr />, document.querySelector('#root'));
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      guitarists: [],
+      guitars: [],
+    };
+  }
+  async componentDidMount() {
+    const guitaristResponse = await axios.get("/api/guitarists");
+    this.setState({
+      guitarists: guitaristResponse.data,
+    });
+  }
+
+  render() {
+    const guitarists = this.state.guitarists;
+    return (
+      <div id="body">
+        <h1>Robby's Guitar List</h1>
+        <h2>Guitarists:</h2>
+        <ul>
+          {guitarists.map((guitarist) => {
+            return <li> {guitarist.name}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.querySelector("#root"));
